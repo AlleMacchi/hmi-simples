63ffad4052e8aa23847d4c232ab6c52aec74a9e5
Mario <polmy015@mymail.unisa.edu.au>
2024-04-11 11:24:00 +0800

    Sidebar Read and Write

---
+++ assets/Esempio/sidebar Funzionante.js
@@ -0,0 +1,56 @@
+// // Toggle sidebar and update mode
+// const btnMode = document.getElementById("btn_Mode");
+// const sidebarMode = document.querySelector(".sidebarMode");
+
+// function toggleSidebar(open) {
+//   if (open) {
+//     sidebarMode.classList.add("open");
+//   } else {
+//     sidebarMode.classList.remove("open");
+//   }
+// }
+
+// btnMode.addEventListener("click", function () {
+//   // Toggle sidebar on mode button click
+//   const isOpen = sidebarMode.classList.contains("open");
+//   toggleSidebar(!isOpen);
+
+//   btnMode.textContent = isOpen ? "MODE" : "X";
+// });
+
+
+// // BUTTON FORWARD
+// $("#btnUpFwdOn").mousedown(function(){
+//   url="IOWriteForward.html";
+//   name='"HMI_PLC".FromHMI.Command.UpFwdOn';
+//   val=1;
+//   sdata=escape(name)+'='+val;
+//   $.post(url,sdata,function(result){});
+// });
+
+// $("#btnUpFwdOn").mouseup(function(){
+//   url="IOWriteForward.html";
+//   name='"HMI_PLC".FromHMI.Command.UpFwdOn';
+//   val=0;
+//   sdata=escape(name)+'='+val;
+//   $.post(url,sdata,function(result){});
+// });
+
+// // BUTTON BACKWARD
+// $("#btnDownBwdOff").mousedown(function(){
+//   url="IOWriteBackward.html";
+//   name='"HMI_PLC".FromHMI.Command.DownBwdOff';
+//   val=1;
+//   sdata=escape(name)+'='+val;
+//   $.post(url,sdata,function(result){});
+// });
+
+// $("#btnDownBwdOff").mouseup(function(){
+//   url="IOWriteBackward.html";
+//   name='"HMI_PLC".FromHMI.Command.DownBwdOff';
+//   val=0;
+//   sdata=escape(name)+'='+val;
+//   $.post(url,sdata,function(result){});
+// });
+
+
