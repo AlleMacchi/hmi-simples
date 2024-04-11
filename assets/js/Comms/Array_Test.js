8a6f96fa99cf000749ee55134a095a9bd7a88679
Mario <polmy015@mymail.unisa.edu.au>
2024-04-11 11:25:06 +0800

    Remove Test data

--- assets/js/Comms/Array_Test.js
+++ assets/js/Comms/Array_Test.js
@@ -1,124 +1,124 @@
 
-//=====================
-// Test purpose
-//=====================
+// //=====================
+// // Test purpose
+// //=====================
 
-setInterval(
-    //Test
-    () => {
-      Array_1 = 100;
-      Array_2 = 100;
-      Array_3 = 100;
-      Array_4 = 100;
-      Array_5 = 100;
-      Array_6 = 100;
-      Array_7 = 100;
-      Array_8 = 100;
-      Array_9 = 100;
-      Array_10 = 100;
-      Array_11 = 100;
-      Array_12 = 100;
-      Array_13 = 100;
-      Array_14 = 100;
-      Array_15 = 100;
-      Array_16 = 100;
-      Array_17 = 100;
-      Array_18 = 100;
-      Array_19 = 100;
-      Array_20 = 100;
-      Array_21 = 100;
-      Array_22 = 100;
-      Array_23 = 100;
-      Array_24 = 100;
-      Array_25 = 100;
-      Array_26 = 100;
-      Array_27 = 100;
-      Array_28 = 100;
-      Array_29 = 100;
-      Array_30 = 100;
-      Array_31 = 100;
-      Array_32 = 100;
-      Array_33 = 100;
-      Array_34 = 100;
-      Array_35 = 100;
-      Array_36 = 100;
-      Array_37 = 100;
-      Array_38 = 100;
-      Array_39 = 100;
-      Array_40 = 100;
-      Array_41 = 100;
-      Array_42 = 100;
-      Array_43 = 0;
-      Array_44 = 100;
-      Array_45 = 100;
-      Array_46 = 100;
-      Array_47 = 100;
-      Array_48 = 100;
-      Array_49 = 100;
-      Array_50 = 100;
-      Array_51 = 100;
+// setInterval(
+//     //Test
+//     () => {
+//       Array_1 = 100;
+//       Array_2 = 100;
+//       Array_3 = 100;
+//       Array_4 = 100;
+//       Array_5 = 100;
+//       Array_6 = 100;
+//       Array_7 = 100;
+//       Array_8 = 100;
+//       Array_9 = 100;
+//       Array_10 = 100;
+//       Array_11 = 100;
+//       Array_12 = 100;
+//       Array_13 = 100;
+//       Array_14 = 100;
+//       Array_15 = 100;
+//       Array_16 = 100;
+//       Array_17 = 100;
+//       Array_18 = 100;
+//       Array_19 = 100;
+//       Array_20 = 100;
+//       Array_21 = 100;
+//       Array_22 = 100;
+//       Array_23 = 100;
+//       Array_24 = 100;
+//       Array_25 = 100;
+//       Array_26 = 100;
+//       Array_27 = 100;
+//       Array_28 = 100;
+//       Array_29 = 100;
+//       Array_30 = 100;
+//       Array_31 = 100;
+//       Array_32 = 100;
+//       Array_33 = 100;
+//       Array_34 = 100;
+//       Array_35 = 100;
+//       Array_36 = 100;
+//       Array_37 = 100;
+//       Array_38 = 100;
+//       Array_39 = 100;
+//       Array_40 = 100;
+//       Array_41 = 100;
+//       Array_42 = 100;
+//       Array_43 = 0;
+//       Array_44 = 100;
+//       Array_45 = 100;
+//       Array_46 = 100;
+//       Array_47 = 100;
+//       Array_48 = 100;
+//       Array_49 = 100;
+//       Array_50 = 100;
+//       Array_51 = 100;
   
-      // updateSpeedLabels();
-      // updateHeaderStatus();
-    },
-    1000
-  );
+//       // updateSpeedLabels();
+//       // updateHeaderStatus();
+//     },
+//     1000
+//   );
   
-  //Test
-  setInterval(() => {
-    Array_1 = 300;
-    Array_2 = 100;
-    Array_3 = 100;
-    Array_4 = 100;
-    Array_5 = 100;
-    Array_6 = 100;
-    Array_7 = 100;
-    Array_8 = 100;
-    Array_9 = 100;
-    Array_10 = 100;
-    Array_11 = 100;
-    Array_12 = 100;
-    Array_13 = 100;
-    Array_14 = 100;
-    Array_15 = 100;
-    Array_16 = 100;
-    Array_17 = 100;
-    Array_18 = 100;
-    Array_19 = 100;
-    Array_20 = 100;
-    Array_21 = 100;
-    Array_22 = 100;
-    Array_23 = 100;
-    Array_24 = 100;
-    Array_25 = 100;
-    Array_26 = 100;
-    Array_27 = 100;
-    Array_28 = 100;
-    Array_29 = 100;
-    Array_30 = 100;
-    Array_31 = 100;
-    Array_32 = 100;
-    Array_33 = 100;
-    Array_34 = 100;
-    Array_35 = 100;
-    Array_36 = 100;
-    Array_37 = 100;
-    Array_38 = 100;
-    Array_39 = 100;
-    Array_40 = 100;
-    Array_41 = 100;
-    Array_42 = 100;
-    Array_43 = 50.0;
-    Array_44 = 100;
-    Array_45 = 100;
-    Array_46 = 100;
-    Array_47 = 100;
-    Array_48 = 100;
-    Array_49 = 100;
-    Array_50 = 100;
-    Array_51 = 100;
+//   //Test
+//   setInterval(() => {
+//     Array_1 = 300;
+//     Array_2 = 100;
+//     Array_3 = 100;
+//     Array_4 = 100;
+//     Array_5 = 100;
+//     Array_6 = 100;
+//     Array_7 = 100;
+//     Array_8 = 100;
+//     Array_9 = 100;
+//     Array_10 = 100;
+//     Array_11 = 100;
+//     Array_12 = 100;
+//     Array_13 = 100;
+//     Array_14 = 100;
+//     Array_15 = 100;
+//     Array_16 = 100;
+//     Array_17 = 100;
+//     Array_18 = 100;
+//     Array_19 = 100;
+//     Array_20 = 100;
+//     Array_21 = 100;
+//     Array_22 = 100;
+//     Array_23 = 100;
+//     Array_24 = 100;
+//     Array_25 = 100;
+//     Array_26 = 100;
+//     Array_27 = 100;
+//     Array_28 = 100;
+//     Array_29 = 100;
+//     Array_30 = 100;
+//     Array_31 = 100;
+//     Array_32 = 100;
+//     Array_33 = 100;
+//     Array_34 = 100;
+//     Array_35 = 100;
+//     Array_36 = 100;
+//     Array_37 = 100;
+//     Array_38 = 100;
+//     Array_39 = 100;
+//     Array_40 = 100;
+//     Array_41 = 100;
+//     Array_42 = 100;
+//     Array_43 = 50.0;
+//     Array_44 = 100;
+//     Array_45 = 100;
+//     Array_46 = 100;
+//     Array_47 = 100;
+//     Array_48 = 100;
+//     Array_49 = 100;
+//     Array_50 = 100;
+//     Array_51 = 100;
   
-    // updateSpeedLabels();
-    // updateHeaderStatus();
-  }, 2000);
+//     // updateSpeedLabels();
+//     // updateHeaderStatus();
+//   }, 2000);
   
